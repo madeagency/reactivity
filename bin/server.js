5944248768c0f65bb1c4b9290a93fbd4cdab7a81
Dawid van der Hoven <dawidvdh@gmail.com>
2017-03-05 12:59:42 +0200

    eslint fixes

--- bin/server.js
+++ bin/server.js
@@ -1,14 +1,15 @@
 import http from 'http'
 import app from '../src/server'
+
 const server = http.createServer(app)
 let currentApp = app
 
-server.listen(3500);
+server.listen(3000)
 
 if (module.hot) {
-	module.hot.accept('../src/server', () => {
-		server.removeListener('request', currentApp)
-		server.on('request', app)
-		currentApp = app
-	})
+  module.hot.accept('../src/server', () => {
+    server.removeListener('request', currentApp)
+    server.on('request', app)
+    currentApp = app
+  })
 }
