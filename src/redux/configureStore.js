b2c733db18db9367cbb0ca46d28717844a85760d
Dawid van der Hoven <dawidvdh@gmail.com>
2017-03-07 23:12:02 +0200

    started implementing hot reloading for redux

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -6,5 +6,15 @@ const epicMiddleware = createEpicMiddleware(rootEpic)
 
 export default function configureStore() {
   const store = createStore(app, applyMiddleware(epicMiddleware))
+
+  if (module.hot) {
+    module.hot.accept(
+      './reducers',
+      () => {
+        store.replaceReducer(require('./reducers').default)
+      }
+    )
+  }
+
   return store
 }
