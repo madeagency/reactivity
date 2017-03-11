ac3874c90578b4c2657a66f23a45709f7754494b
Dawid van der Hoven <dawidvdh@gmail.com>
2017-03-12 00:17:00 +0200

    added devtools and resolved hot reload bug

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,11 +1,16 @@
-import { createStore, applyMiddleware } from 'redux'
+import { createStore, applyMiddleware, compose } from 'redux'
 import { createEpicMiddleware } from 'redux-observable'
 import app, { rootEpic } from './reducers'
 
 const epicMiddleware = createEpicMiddleware(rootEpic)
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+
+const enhancer = composeEnhancers(
+  applyMiddleware(epicMiddleware)
+)
 
 export default function configureStore() {
-  const store = createStore(app, applyMiddleware(epicMiddleware))
+  const store = createStore(app, enhancer)
 
   if (module.hot) {
     module.hot.accept(
