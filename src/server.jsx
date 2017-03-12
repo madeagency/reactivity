8d1fb415d9bb1c05061a9bd870653d46f3730460
Dawid van der Hoven <dawidvdh@gmail.com>
2017-03-12 16:54:36 +0200

    linting error

--- src/server.jsx
+++ src/server.jsx
@@ -1,10 +1,10 @@
 import express from 'express'
 import React from 'react'
 import { Provider } from 'react-redux'
-import configureStore from './redux/configureStore'
 import { withAsyncComponents } from 'react-async-component'
 import { renderToString, renderToStaticMarkup } from 'react-dom/server'
 import { StaticRouter } from 'react-router-dom'
+import configureStore from './redux/configureStore'
 import Html from './helpers/Html'
 import App from './containers/App/App'
 
