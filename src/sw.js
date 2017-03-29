adf5b6ccbb738cde7096f6da2e5fecea4d0a645f
Dawid van der Hoven <dawidvdh@gmail.com>
2017-03-29 13:47:05 +0200

    sample of logging out files

--- src/sw.js
+++ src/sw.js
@@ -1,3 +1,12 @@
-self.addEventListener('install', (event) => {
-  console.log('INSTALLING', event)
+const { assets } = global.serviceWorkerOption
+
+const CACHE_NAME = (new Date()).toISOString()
+
+const assetsToCache = [
+  ...assets,
+  '/'
+].map(path => new URL(path, global.location).toString())
+
+self.addEventListener('install', () => {
+  console.log(CACHE_NAME, assetsToCache)
 })
