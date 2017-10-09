6e3307ea9fbe2764d9f8c57717d2ad5c6dab3d78
Dawid van der Hoven <dawidvdh@gmail.com>
2017-10-09 20:12:27 +0200

    single quotes

--- webpack/client.prod.js
+++ webpack/client.prod.js
@@ -1,102 +1,102 @@
-const path = require("path")
-const webpack = require("webpack")
-const ExtractCssChunks = require("extract-css-chunks-webpack-plugin")
-const Dotenv = require("dotenv-webpack")
-const autoprefixer = require("autoprefixer")
-const ServiceWorkerPlugin = require("serviceworker-webpack-plugin")
-const BundleAnalyzerPlugin = require("webpack-bundle-analyzer")
+const path = require('path')
+const webpack = require('webpack')
+const ExtractCssChunks = require('extract-css-chunks-webpack-plugin')
+const Dotenv = require('dotenv-webpack')
+const autoprefixer = require('autoprefixer')
+const ServiceWorkerPlugin = require('serviceworker-webpack-plugin')
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer')
   .BundleAnalyzerPlugin
 
 module.exports = {
-  name: "client",
-  target: "web",
-  devtool: "hidden-source-map",
-  entry: [path.resolve(__dirname, "../src/client.js")],
+  name: 'client',
+  target: 'web',
+  devtool: 'hidden-source-map',
+  entry: [path.resolve(__dirname, '../src/client.js')],
   output: {
-    filename: "[name].[chunkhash].js",
-    chunkFilename: "[name].[chunkhash].js",
-    path: path.resolve(__dirname, "../build"),
-    publicPath: "/",
+    filename: '[name].[chunkhash].js',
+    chunkFilename: '[name].[chunkhash].js',
+    path: path.resolve(__dirname, '../build'),
+    publicPath: '/'
   },
   module: {
     rules: [
       {
         test: /\.jsx?$/,
         exclude: /node_modules/,
-        use: "babel-loader",
+        use: 'babel-loader'
       },
       {
         test: /\.scss$/,
         use: ExtractCssChunks.extract({
           use: [
             {
-              loader: "css-loader",
+              loader: 'css-loader',
               query: {
                 modules: true,
-                localIdentName: "[name]__[local]--[hash:base64:5]",
-              },
+                localIdentName: '[name]__[local]--[hash:base64:5]'
+              }
             },
-            "sass-loader",
+            'sass-loader',
             {
-              loader: "postcss-loader",
+              loader: 'postcss-loader',
               options: {
-                plugins: () => [autoprefixer({ browsers: "last 2 versions" })],
-              },
-            },
-          ],
-        }),
+                plugins: () => [autoprefixer({ browsers: 'last 2 versions' })]
+              }
+            }
+          ]
+        })
       },
       {
         test: /\.(jpg|png|gif|svg|ico)$/,
         use: [
           {
-            loader: "url-loader",
-          },
-        ],
-      },
-    ],
+            loader: 'url-loader'
+          }
+        ]
+      }
+    ]
   },
   resolve: {
     alias: {
-      components: path.resolve(__dirname, "..", "src/components/"),
-      reducers: path.resolve(__dirname, "..", "src/redux/reducers/"),
+      components: path.resolve(__dirname, '..', 'src/components/'),
+      reducers: path.resolve(__dirname, '..', 'src/redux/reducers/')
     },
-    extensions: [".json", ".js", ".jsx"],
+    extensions: ['.json', '.js', '.jsx']
   },
   plugins: [
     new BundleAnalyzerPlugin({
-      analyzerMode: "disabled",
-      generateStatsFile: true,
+      analyzerMode: 'disabled',
+      generateStatsFile: true
     }),
     new ExtractCssChunks(),
     new webpack.optimize.CommonsChunkPlugin({
-      names: ["bootstrap"], // needed to put webpack bootstrap code before chunks
-      filename: "[name].[chunkhash].js",
-      minChunks: Infinity,
+      names: ['bootstrap'], // needed to put webpack bootstrap code before chunks
+      filename: '[name].[chunkhash].js',
+      minChunks: Infinity
     }),
     new Dotenv({
-      path: path.resolve(__dirname, "../.env"),
+      path: path.resolve(__dirname, '../.env'),
       systemvars: true,
-      safe: false,
+      safe: false
     }),
     new webpack.optimize.UglifyJsPlugin({
       compress: {
         screw_ie8: true,
-        warnings: false,
+        warnings: false
       },
       mangle: {
-        screw_ie8: true,
+        screw_ie8: true
       },
       output: {
         screw_ie8: true,
-        comments: false,
+        comments: false
       },
-      sourceMap: true,
+      sourceMap: true
     }),
     new ServiceWorkerPlugin({
-      entry: path.join(__dirname, "..", "src/sw.js"),
-      excludes: ["*hot-update*", "**/*.map", "**/stats.json"],
+      entry: path.join(__dirname, '..', 'src/sw.js'),
+      excludes: ['*hot-update*', '**/*.map', '**/stats.json']
     }),
-    new webpack.HashedModuleIdsPlugin(), // not needed for strategy to work (just good practice)
-  ],
+    new webpack.HashedModuleIdsPlugin() // not needed for strategy to work (just good practice)
+  ]
 }
