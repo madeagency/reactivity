84f854fc374dc51df7e050ab0fe8e4f839537501
Dawid van der Hoven <dawidvdh@gmail.com>
2017-12-30 18:02:56 +0200

    revert away from hot-reloader beta

--- webpack/client.dev.js
+++ webpack/client.dev.js
@@ -13,6 +13,7 @@ module.exports = {
   devtool: 'eval',
   entry: [
     'webpack-hot-middleware/client?path=/__webpack_hmr&timeout=20000&reload=false&quiet=false&noInfo=false',
+    'react-hot-loader/patch',
     path.resolve(__dirname, '../src/client.js')
   ],
   output: {
